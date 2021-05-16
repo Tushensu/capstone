4a99581d8ba62cda1ccbe0d54d82cee26abbc329
EC2 Default User <ec2-user@ip-172-31-43-95.eu-west-1.compute.internal>
2021-05-16 13:37:27 +0000

    Modified data sourcing script

--- download_source.sh
+++ download_source.sh
@@ -5,8 +5,8 @@
 # 4. Datahub : Country Codes
 
 # Download & unzip Kaggle dataset
-kaggle datasets downlad -d
-uzip climate-change-earth-surface-temperature-data.zip -d climate-change-dataset
+kaggle datasets download -d berkeleyearth/climate-change-earth-surface-temperature-data
+unzip climate-change-earth-surface-temperature-data.zip -d climate-change-dataset
 
 # Download from GitHub
 wget https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.json -O co2_emission_data.json
@@ -15,4 +15,4 @@ wget https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.json -
 python3 download_world_bank_data.py
 
 # Download from Datahub
-wget https://datahub.io/core/country-codes/r/0.csv -O /country_population/country_codes.csv
+wget https://datahub.io/core/country-codes/r/0.csv -O country_population/country_codes.csv
